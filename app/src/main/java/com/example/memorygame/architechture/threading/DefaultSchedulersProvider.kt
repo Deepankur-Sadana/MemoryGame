b83d4cfe2b9ab10f78fd8772d6a710b92671b67d
DEEPANKUR SADANA <writetodeepankursadana@gmail.com>
2019-06-20 14:30:49 +0530

    adding boiler plate

---
+++ app/src/main/java/com/example/memorygame/architechture/threading/DefaultSchedulersProvider.kt
@@ -0,0 +1,16 @@
+package com.example.cards.architechture.threading
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class DefaultSchedulersProvider : SchedulersProvider {
+  override val io: Scheduler
+    get() = Schedulers.io()
+
+  override val computation: Scheduler
+    get() = Schedulers.computation()
+
+  override val ui: Scheduler
+    get() = AndroidSchedulers.mainThread()
+}
---
+++ app/src/main/java/com/example/memorygame/architechture/threading/SchedulersProvider.kt
@@ -0,0 +1,9 @@
+package com.example.cards.architechture.threading
+
+import io.reactivex.Scheduler
+
+interface SchedulersProvider {
+  val io: Scheduler
+  val computation: Scheduler
+  val ui: Scheduler
+}
