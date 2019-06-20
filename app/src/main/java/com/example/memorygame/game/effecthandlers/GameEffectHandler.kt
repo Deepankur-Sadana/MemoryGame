4f5e0ffe4a2e5461a31572577ac5d875e3686511
DEEPANKUR SADANA <writetodeepankursadana@gmail.com>
2019-06-20 15:25:11 +0530

    bp

---
+++ app/src/main/java/com/example/memorygame/game/effecthandlers/GameEffectHandler.kt
@@ -0,0 +1,68 @@
+package com.example.memorygame.game.effecthandlers
+
+import com.example.cards.architechture.threading.SchedulersProvider
+import com.example.memorygame.game.engine.GameResult
+import com.example.memorygame.game.engine.MoveResult
+import com.example.memorygame.game.*
+import com.spotify.mobius.rx2.RxMobius
+import io.reactivex.Observable
+import io.reactivex.ObservableSource
+import io.reactivex.ObservableTransformer
+import timber.log.Timber
+
+object GameEffectHandler {
+    fun create(
+        gameApi: GameEngine,
+        schedulersProvider: SchedulersProvider
+    ): ObservableTransformer<GameEffect, GameEvent> {
+        return RxMobius
+            .subtypeEffectHandler<GameEffect, GameEvent>()
+            .addTransformer(
+                GetResultEffect::class.java,
+                getMoveResult(gameApi, schedulersProvider)
+            )
+            .build()
+    }
+
+
+    private fun getMoveResult(
+        gitHubApi: GameEngine,
+        schedulersProvider: SchedulersProvider
+    ): ObservableTransformer<GetResultEffect, GameEvent> {
+        return object : ObservableTransformer<GetResultEffect, GameEvent> {
+            override fun apply(
+                getResultEffect: Observable<GetResultEffect>
+            ): ObservableSource<GameEvent> {
+                return getResultEffect
+                    .flatMapSingle { effect ->
+                        gitHubApi
+                            .compareCards(effect.moveRequest)
+                            .map(GameEffectHandler::mapToGameResultEvent)
+                            .doOnError(Timber::e)
+                            .onErrorReturn { mapToErrorEvent(it) }
+                    }
+                    .subscribeOn(schedulersProvider.io)
+            }
+        }
+    }
+
+    private fun mapToGameResultEvent(result: MoveResult): GameEvent {
+        when {
+            result.gameResult == GameResult.WON -> {
+                return GameWonEvent(result.level, result.finalPoints)
+            }
+            result.gameResult == GameResult.ONGOING -> {
+                return FlipCardsEvent(
+                    result.level,
+                    result.finalPoints,
+                    result.flips,
+                    result.visibleCardID
+                )
+            }
+        }
+        return GameCompletedEvent
+    }
+
+    private fun mapToErrorEvent(throwable: Throwable): GameEvent =
+        UnableToFetchResultEvent
+}
