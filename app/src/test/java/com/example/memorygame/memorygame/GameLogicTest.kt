cbbb12ddb4293a664c7382a8f9c5a2089bd3e95d
DEEPANKUR SADANA <writetodeepankursadana@gmail.com>
2019-06-20 15:14:14 +0530

    adding classes and tests for the game

---
+++ app/src/test/java/com/example/memorygame/memorygame/GameLogicTest.kt
@@ -0,0 +1,33 @@
+package com.example.memorygame.memorygame
+
+import com.example.memorygame.game.*
+import com.spotify.mobius.test.UpdateSpec
+
+
+import com.google.common.truth.Truth.assertThat
+import com.spotify.mobius.test.NextMatchers.*
+import com.spotify.mobius.test.UpdateSpec.assertThatNext
+import org.junit.Test
+
+class GameLogicTest {
+
+    private val updateSpec = UpdateSpec<GameModel, GameEvent, GameEffect>(GameLogic::update)
+    private val startGameModel = GameModel.READY_TO_START_GAME_MODEL
+
+
+    @Test
+    fun `when the game is launched, user can start the game`() {
+        val noTileSelectedModel = startGameModel.startTickingGame()
+
+        updateSpec.given(startGameModel)
+            .`when`(StartGameEvent)
+            .then(
+                assertThatNext(
+                    hasModel(noTileSelectedModel),
+                    hasNoEffects()
+                )
+            )
+        assertThat(noTileSelectedModel.currentLevel).isEqualTo(1)
+        assertThat(noTileSelectedModel.selectedCard).isEqualTo(null)
+    }
+}
---
+++ app/src/test/java/com/example/memorygame/memorygame/effecthandlers/GameEffectHandler.kt
@@ -0,0 +1,4 @@
+package com.example.memorygame.memorygame.effecthandlers
+
+class GameEffectHandler {
+}
